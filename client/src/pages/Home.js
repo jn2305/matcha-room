9e7265d185d2a8ccd9a6c716c8cd63a83d510c12
Jasmine Ngo <158764381+jasxlynn@users.noreply.github.com>
2025-07-27 22:09:03 -0400

    added more landing page features

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import '../App.css';
 import matchaImg from '../assets/latte.jpg'
+import popMatcha from '../assets/matcha.jpg'
+import strawbMatcha from '../assets/strawb.jpg'
 import { Link } from 'react-router-dom';
 
 function Home() {
@@ -11,12 +13,12 @@ function Home() {
             <div className="banner">
                 <div className="navbar">
                     <h1 id="store-name">the matcha room</h1>
-                        <ul className="nav-links">
-                            <li><a href="/">home</a></li>
-                            <li><a href="/menu">menu</a></li>
-                            <li><a href="/about">about</a></li>
-                            <li><a href="/contact">contact</a></li>
-                        </ul>
+                    <ul className="nav-links">
+                        <li><a href="/">home</a></li>
+                        <li><a href="/menu">menu</a></li>
+                        <li><a href="/about">about</a></li>
+                        <li><a href="/contact">contact</a></li>
+                    </ul>
                 </div>
             </div>
             <div className="matcha-wrapper">
@@ -32,6 +34,16 @@ function Home() {
             
             <div className="favorites">
                 <h2 id="popular-txt">Our Most Popular</h2>
+                <div className="popImgs">
+                    <div className="pop-img-container">
+                        <img src={popMatcha} alt="Matcha" className="pop-img" />
+                        <p className="caption">Classic Matcha</p>
+                    </div>
+                    <div className="pop-img-container">
+                        <img src={strawbMatcha} alt="Strawberry Matcha" className="pop-img" />
+                        <p className="caption">Strawberry Matcha</p>
+                    </div>
+                </div>
             </div>
         </div>
         
